7d472797ed37a8efe6c57939e53cfe7b681b3cb2
Shakti Kadapure <shakti.kadapure@gmail.com>
2018-06-29 17:51:33 -0400

    improved solution

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const ccmask = require('./ccmasking');
 
-console.log("1 - " + ccmask("abcd-efgh-ijkl-mnop"));
-console.log("2 - " + ccmask("123456"));
-console.log("3 - " + ccmask("12345678901"));
-console.log("4 - " + ccmask("abcd123456787asjdnakj89as"));
-console.log("5 - " + ccmask("1234-SKIP-5678-9101"));
+console.log("1 - " + "abcd-efgh-ijkl-mnop"  + " -> " + ccmask("abcd-efgh-ijkl-mnop"));
+console.log("2 - " + "123456"  + " -> " + ccmask("123456"));
+console.log("3 - " + "12345678901"  + " -> " + ccmask("12345678901"));
+console.log("4 - " + "abcd123456787asjdnakj89as"  + " -> " + ccmask("abcd123456787asjdnakj89as"));
+console.log("5 - " + "1234-SKIP-5678-9101"  + " -> " + ccmask("1234-SKIP-5678-9101"));
+console.log("5 - " + "12345"  + " -> " + ccmask("12345"));
